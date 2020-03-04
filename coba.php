b01fa25f14890e33beab1e72ce600b3e89dc7483
tembeleklincung <61751026+tembeleklincung@users.noreply.github.com>
2020-03-04 09:46:33 +0700

    Update coba.php

--- coba.php
+++ coba.php
@@ -2,36 +2,36 @@
 
 error_reporting(0);
 include ("func.php");
-echo "\e            GOJEK VERSION 1.7.1            \n";
-echo "\e SCRIPT GOJEK AUTO REGISTER + AUTO CLAIM VOUCHER\n";
+echo "\e            sing anyar           \n";
+echo "\e ben cepet nglebokno kodene \n";
 echo "\n";
 nope:
-echo "\e[?] Masukkan Nomor HP Anda : ";
+echo "\e[?] lebok e nomer hpne : ";
 $nope = trim(fgets(STDIN));
 $cek = cekno($nope);
 if ($cek == false)
     {
-    echo "\e[x] Nomor Telah Terdaftar\n";
+    echo "\e[x] Nomor wis tau kanggo\n";
 			goto nope;
     }
   else
     {
-echo "\e[!] Siapkan OTPmu\n";
+echo "\e[!] disiapno otne ojo  lali\n";
 sleep(5);
 $register = register($nope);
 if ($register == false)
     {
-    echo "\e[x] Failed Get OTP!\n";
+    echo "\e[x] ora iso iki!\n";
     }
   else
     {
     otp:
-    echo "\e[!] Masukkan Kode Verifikasi (OTP) : ";
+    echo "\e[!] yuk dilebokno otpne : ";
     $otp = trim(fgets(STDIN));
     $verif = verif($otp, $register);
     if ($verif == false)
         {
-        echo "\e[x] Kode Verifikasi Salah\n";
+        echo "\e[x] Kliru wong ganteng \n";
         goto otp;
         }
       else
@@ -39,11 +39,11 @@ if ($register == false)
 		$h=fopen("newgojek.txt","a");
 		fwrite($h,json_encode(array('token' => $verif, 'voc' => 'gofood gak ada'))."\n");
 		fclose($h); 
-                echo "\e[!] Trying to redeem Reff : G-75SR565 !\n";
+                echo "\e[!] Trying to redeem Reff : G-43CGVQX !\n";
                 sleep(3);
             $claim = reff($verif);
             if ($claim == false){
-            echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+            echo "\e[!] ora iso diclaim 😭\n";
             }else{
                 echo "\e[+] ".$claim."\n";
             }
